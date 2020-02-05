d5aa1e546e17f155702c0d18e1c02adaa4270e79
awongimc <awong@imcba.com>
2020-02-05 16:32:06 -0500

    Comments and not.toBe

--- functions.test.js
+++ functions.test.js
@@ -2,6 +2,17 @@ const functions = require('./functions');
 
 test('Add 2 + 2 to equal 4', () => {
     //expect: add function with expected parameters, pass in data
-    //then add matcher for jest: 
+    //then add matcher for jest (toBe)
     expect(functions.add(2, 2)).toBe(4);
 });
+
+test('Add 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
