6e1a99af5de57dbfb0a8c14aeca5566317e80467
awongimc <awong@imcba.com>
2020-02-06 09:42:40 -0500

    testing load and regex

--- functions.test.js
+++ functions.test.js
@@ -1,8 +1,8 @@
 const functions = require('./functions');
 
 test('Add 2 + 2 to equal 4', () => {
-    //expect: add function with expected parameters, pass in data
-    //then add matcher for jest (toBe)
+    // expect: add function with expected parameters, pass in data
+    // then add matcher for jest (toBe)
     expect(functions.add(2, 2)).toBe(4);
 });
 
@@ -17,17 +17,18 @@ test('Add 2 + 2 to NOT equal 5', () => {
 // toBeTruthy matches anything that an if statement treats as true
 // toBeFalsy matches anything that an if statement treats as false
 
-//checking for null
+// checking for null
 test('Should be null', () => {
     expect(functions.isNull()).toBeNull();
 });
 
-//should be falsy, ex. null, 0, undefined
+// should be falsy, ex. null, 0, undefined
 test('Should be falsy', () => {
     expect(functions.checkValue(undefined)).toBeFalsy();
 });
 
-//toBe can't be used on an object, different memory spaces even if the object looks the same visually
+// toEqual
+// toBe can't be used on an object, different memory spaces even if the object looks the same visually
 test('User should be Jester Lavorre object', () => {
     expect(functions.createUser()).toEqual({
         firstName: 'Jester',
@@ -35,3 +36,16 @@ test('User should be Jester Lavorre object', () => {
     });
 });
 
+//Less than and greate than
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+});
+
+// Regular expressions
+test('There is no I in cheeto', () => {
+    expect('cheeto').not.toMatch(/I/);
+});
+
+// Arrays 
